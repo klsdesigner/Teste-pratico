e3c8b511f7f9e26a59e0fb0a38f0b0547a293e55
Kleber de Souza <klsdesigner@hotmail.com>
2020-10-30 12:55:02 -0300

    Alteração nas rotas

--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-
-// Route::get('/painel/index', 'Painel\PainelController@index')->name('painel.index');
-
-// Route::get('/painel.user', 'Painel\UserController@index')->name('user.index');
-
-// Route::get('Painel/veiculos', 'Painel\VeiculosController@index')->name('veiculos.index');
-// Route::get('Painel/veiculos/create', 'Painel\VeiculosController@create')->name('veiculos.create');
-// Route::post('Painel/veiculos/store', 'Painel\VeiculosController@store')->name('veiculos.store');
-// Route::get('Painel/veiculos/{id}/edit', 'Painel\VeiculosController@edit')->name('veiculos.edit');
-// Route::put('Painel/veiculos/update', 'Painel\VeiculosController@update')->name('veiculos.update');
-// Route::post('Painel/veiculos/destroy/{id}', 'Painel\VeiculosController@destroy')->name('veiculos.destroy');
-
 Route::group(['prefix' => 'Painel', 'namespace' => 'Painel'], function () {
 
     Route::get('/index', 'PainelController@index')->name('painel.index');
@@ -45,29 +33,9 @@ Route::group(['prefix' => 'Painel', 'namespace' => 'Painel'], function () {
     Route::post('veiculos/store', 'VeiculosController@store')->name('veiculos.store');
     Route::get('veiculos/{id}/edit', 'VeiculosController@edit')->name('veiculos.edit');
     Route::put('veiculos/update', 'VeiculosController@update')->name('veiculos.update');
-    
+
     Route::post('veiculos/destroy/{id}', 'VeiculosController@destroy')->name('veiculos.destroy');
 
     
 });
 
-
-
-
-// Route::resource('painel/user', 'UserController')->names( 'painel.user');
-// Route::resource('painel/veiculo', 'VeiculoController')->names('painel.veiculo');
-
-// Route::group(['prefix'=>'admin', 'as'=>'admin.'], function () {
-//     //Authentication Rotes
-//     $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-//     $this->post('login', 'Auth\LoginController@login');
-//     $this->post('logout', 'Auth\LoginController@logout')->name('logout');
-
-//     //Password Reset
-//     $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-//     $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-//     $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-//     $this->post('password/reset', 'Auth\ResetPasswordController@reset');
-
-//     Route::get('/home', 'HomeController@index')->name('home');
-// });
